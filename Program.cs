6a62b874aa6327ada7f6e2926467dcf77767947b
Valentina Zarechneva <valvas_07@mail.ru>
2023-09-20 03:13:07 +0300

    задача на выведение четного числа

--- Program.cs
+++ Program.cs
@@ -1,27 +1,13 @@
-﻿// Задача 4: 
-// *Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.*
-// Например: 
-// * 2, 3, 7 -> 7
-// * 44 5 78 -> 78
-// * 22 3 9 -> 22
+﻿// Задача 6: Напишите программу, которая на вход 
+// принимает число и выдаёт, является ли число чётным 
+// (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
 
 Console.Write("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число: ");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число: ");
-int c = Convert.ToInt32(Console.ReadLine());
-int max = a;
-if(a > max) max = a;
-if(b > max) max = b;
-if(c > max) max = c;
-
-Console.Write("max = ");
-Console.WriteLine(max);
-
-
-Console.WriteLine(b);
-if (b < c)
-Console.WriteLine(c);
-
-
+int n = Convert.ToInt32(Console.ReadLine());
+if ( n % 2 == 0)
+Console.WriteLine("четное число");
+else 
+Console.Write("нечетное число");
